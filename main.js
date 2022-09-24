3dca26b72f6b1472e758da32720978059a2f2936
moustafargbpro <moustafargbpro@outlook.com>
2022-09-24 22:17:27 +0200

    using git method

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express() ;
+
+app.get('/' , (req , res) => {
+    // res.write(`<h1>Hello there</h1>`)
+    // res.send(`<h1>Hello there</h1>`)
+    // res.send(`<h1>Home Page</h1>`);
+    res.json({message : 'this is a message'});
+    // res.download("main.js");
+    // res.end();
+})
+
+app.get('/users' , (req , res) => {
+    res.send("Users List") ;
+    res.end();
+})
+
+app.listen(5000) ;
