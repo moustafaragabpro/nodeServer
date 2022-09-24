6790b2385bc269851119e15d19494ce95265792b
moustafargbpro <moustafargbpro@outlook.com>
2022-09-24 22:02:22 +0200

    remove old files and installing express, nodemon

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-const route = require("./routes");
-
-console.log(route.text);
-const server = http.createServer(route.routeHandler);
-
-server.listen(3000, () => console.log("connected"));
